7acfe3e978cd3e7c5b31ec8d85194fba71f43a42
Lings <1052010391@qq.com>
2017-01-25 17:09:02 +0800

    add send email function

---
+++ SMTP.py
@@ -0,0 +1,31 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+class MailFormat():
+	sender = ''
+	def __init__(self):
+		self.server = smtplib.SMTP('',25)
+		self.server.starttls()
+		self.server.set_debuglevel(1)
+		self.server.login(self.sender,'')
+
+	def setMessage(self,**msgcont):
+		self.message = MIMEText(msgcont['cont'],msgcont['mimetype'],'utf-8')
+		self.message['From'] = self.sender
+		self.message['Subject'] = msgcont['subject']
+
+	def sendMessage(self,receivers):
+		try:
+			self.message['To'] = receivers
+			self.server.sendmail(self.sender,receivers,self.message.as_string())
+			print("邮件发送成功")
+		except smtplib.SMTPException as err:
+			print("Error: 无法发送邮件",err)
+			
+# if __name__ == '__main__':
+# 	mail = MailFormat()
+# 	mail.setMessage(cont="i am roboot邮件发送测试",
+# 					subject="python 邮件测试",
+# 					mimetype="plain")
+# 	print(mail.message)
+# 	mail.sendMessage('')
